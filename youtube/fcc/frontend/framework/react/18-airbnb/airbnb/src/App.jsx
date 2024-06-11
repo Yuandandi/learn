8dba80f3dac7097a4dadfd0c84aa77870e7b987d
Yuandandi <yoan.yoan.56829@gmail.com>
2024-06-11 03:37:55 +0000

    horizontal scrolling

--- youtube/fcc/frontend/framework/react/18-airbnb/airbnb/src/App.jsx
+++ youtube/fcc/frontend/framework/react/18-airbnb/airbnb/src/App.jsx
@@ -5,19 +5,34 @@ import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Card from './components/Card.jsx'
+import data from './data.js'
 
 
 function App() {
-    return (
-        <Card
-        img="../public/Katie-Zaferes.jpg"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Live_Lessons with Katie Zaferes"
-        price={136}
-    />
-    )
+    const cards = data.map(item => { // everytime you use .map() will appear an error child key, so you need to specify key attribute and passing unique value, in this case "id" as we know id is the only unique value
+        return (
+            <Card
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            country={item.location}
+            title={item.title}
+            price={item.price}
+        />
+        )
+    })
+
+  return (
+    <div>
+      <Navbar />
+      <section className="cards-list">
+        {cards}
+      </section>
+    </div>
+  )
 }
+
+
 
 export default App
--- youtube/fcc/frontend/framework/react/18-airbnb/airbnb/src/data.js
+++ youtube/fcc/frontend/framework/react/18-airbnb/airbnb/src/data.js
@@ -1,10 +1,10 @@
 export default [
     {
         id: 1,
-        title: "learn wedding photography",
+        title: "life lessons with Katie Zafares",
         description: "but learning from those difficult moments is What gave me the ability to beyond above them and reach my goals",
         price: 136,
-        coverImg: "katie-zaferes.png",
+        coverImg: "../public/Katie-Zaferes.jpg",
         stats: {
             rating: 5.0,
             reviewCount: 6,
@@ -17,7 +17,7 @@ export default [
         title: "learn wedding photography",
         description: "interested in becoming a wedding photographer?",
         price: 125,
-        coverImg: "wedding-photography.png",
+        coverImg: "../public/wedding.jpg",
         stats: {
             rating: 5.0,
             reviewCount: 30,
@@ -30,7 +30,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful norwegian landscape and meet new firends all whild conquering rugged terrain on your montain bike. (Bike provided)",
         price: 50,
-        coverImg: "mountain-bike.png",
+        coverImg: "../public/bike.jpg",
         stats: {
             rating: 4.6,
             reviewCount: 3,
