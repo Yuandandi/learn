0633e723d3ea91e9260aa0faa163d9855f52dd4f
Yuandandi <yoan.yoan.56829@gmail.com>
2024-07-01 06:51:54 +0000

    dave

---
+++ ljs/dave/12hr/01-console/script.js
@@ -0,0 +1,26 @@
+// // console.log(){{{
+// // object.method(Argument);
+// console.log(typeof console); // object
+// console.log(typeof 90); // number
+// console.log(90); // 90 purple color
+// console.log("foo"); // foo white color
+// console.log(20, "Hello", true); // 20 "Hello" true
+
+// // set variable;
+// const x = 100;
+// console.log(x); // 100}}}
+// // console.error(){{{
+// console.error("Alert") // Alert, with the red color}}}
+// // console.warn(){{{
+// console.warn("Warning"); // Warning, yellow color}}}
+// // console.table(){{{
+// console.table({ name: "foo", email: "brad@foo.com" }) // object, table form in console, nice and  neat}}}
+// // console.group() and console.groupEnd(){{{
+// console.group("simple things") // start group console
+// console.log("foo");
+// console.error("alert");
+// console.warn('warning');
+// console.groupEnd(); // end group console}}}
+// // applying style within the console {{{
+// const styles = 'padding: 10px; background-color: white; color: green';
+// console.log("%cHello World", styles);// }}}
---
+++ ljs/dave/12hr/02-variables-data-types/script.js
@@ -0,0 +1,30 @@
+// // Ways to Declare a Variable{{{
+// // var, let, const
+
+// let firstName = "John";
+// let lastName = "Doe";
+// console.log(firstName, lastName)
+// // console.log(firstName, lastName, age) // initialize error
+
+// let age = 30
+// console.log(age);// }}}
+// // Naming Conventions{{{
+// // - Only letters, numbers, underscore and dollar signs
+// // - Can't start with number;
+
+// // let 1name = "foo"; // invalid or unexpected token
+// // let name+ = "foo"; // unexpected token
+// let name1 = "foo"; // works}}}
+// Multi-Word Formattting{{{
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase}}}
+// Re-Assigning Variable{{{{{{
+let age = 30;
+age = 40;
+console.log(age);// }}}
+// declare only{{{
+let nama;
+nama = "foo";
+console.log(nama);// }}}}}}
